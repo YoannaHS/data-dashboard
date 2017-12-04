142a124722b7af9fcbba910f70a4ef1cf7497d1f
YoannaHS <yoannahernandez.s@gmail.com>
2017-12-04 13:27:39 -0500

    Añadiendo estructura de carpetas

--- js/app.js
+++ js/app.js
@@ -1,6 +1,42 @@
-/*
- * Funcionalidad de tu producto
- */
 
-// Puedes hacer uso de la base de datos a través de la variable `data`
-console.log(data);
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawChart);
+
+function drawChart() {
+
+  var data = google.visualization.arrayToDataTable([
+    ['Sedes', 'Total de alumnas'],
+    ['Arequipa', 30],
+    ['Santiago de Chile', 95],
+    ['Ciudad de México', 70],
+    ['Lima', 66],
+  ]);
+
+  var options = {
+    title: 'Total de alumnas por sedes'
+  };
+
+  var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+
+  chart.draw(data, options);
+
+  google.charts.load('current', {packages:['corechart']});
+  google.charts.setOnLoadCallback(drawChart);
+  function drawChart() {
+    var data = google.visualization.arrayToDataTable([
+      ['Desercion', 'Total y porcentaje'],
+      ['Arequipa', 51],
+      ['Santiago de Chile', 27],
+      ['Ciudad de México', 18],
+      ['Lima', 59],
+    ]);
+
+    var options = {
+      title: 'Total de alumnas que desertaron por sede',
+      pieHole: 0.4,
+    };
+
+    var chart = new google.visualization.PieChart(document.getElementById('donutchart'));
+    chart.draw(data, options);
+  };
+};
