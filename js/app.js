8628a6d57b24c548c1b2fe5c79af060d5c1f0ab3
SteffAhv <tefavv22@gmail.com>
2017-12-04 14:52:00 -0500

    js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
 
+
+
 google.charts.load('current', {'packages':['corechart']});
 google.charts.setOnLoadCallback(drawChart);
 
@@ -40,3 +42,48 @@ function drawChart() {
     chart.draw(data, options);
   };
 };
+
+//Funcion de los tabs de main section
+var showHide = function(e) {
+  var tabSelected = e.target.dataset.tabSelected;
+  var overview = document.getElementById('container-overview');
+  var students = document.getElementById('container-students');
+  var teachers = document.getElementById('container-teachers');
+
+  if(tabSelected === 'tabOverview') {
+      console.log('This is overview section');
+      //ocultar students, teachers  
+      students.style.display ='none';
+      teachers.style.display ='none';
+      // mostrar solo overview
+      overview.style.display = 'block';
+  }else if(tabSelected === 'tabStudents') {
+      console.log('This is students section');
+      //ocultar overview, teachers
+      overview.style.display ='none';
+      teachers.style.display ='none';
+      // mostrar solo students
+      students.style.display ='block';
+  }else if(tabSelected === 'tabTeachers') {
+      console.log('This is teachers section');
+      //ocultar overview, students 
+      overview.style.display ='none';
+      students.style.display ='none';
+      // mostrar solo teachers
+      teachers.style.display = 'block';
+  }
+}
+
+var loadPage = function(){
+  var overview = document.getElementById('container-overview');
+  var students = document.getElementById('container-students');
+  var teachers = document.getElementById('container-teachers'); 
+  overview.style.display ='none';
+  students.style.display ='none';
+  teachers.style.display ='none';
+  var tabElements = document.getElementsByClassName('tab');
+  for(var i = 0;i < tabElements.length; i++) {
+      tabElements[i].addEventListener('click', showHide)
+  }
+}
+loadPage();
